7f804d6b9fb88d5eabc03db55b85417a4e58aed6
L1628 <L1628@OHJE-330-WS-29>
2017-02-23 14:47:12 +0200

    Demo07

---
+++ teht000/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ViewManagement;
+using System.Diagnostics;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace teht000
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchWindowingMode
+                = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+
+            App.Current.DebugSettings.EnableFrameRateCounter = false;
+        }
+
+        private void savebutton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Name" + nametextbox.Text);
+            Debug.WriteLine("Email" + emailtextbox.Text);
+            if ((bool)normalusercheckbox.IsChecked)
+            {
+                Debug.WriteLine("Normal user!");
+            }
+        }
+    }
+}
